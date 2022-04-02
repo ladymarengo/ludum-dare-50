a58d7e145741bdfa728ba078af87e354dbebee13
Natalia Samoilova <samoilova.natalia.n@gmail.com>
2022-04-02 18:40:35 +0300

    Basic cat behavior

--- src/cat.rs
+++ src/cat.rs
@@ -1,7 +1,7 @@
-use bevy::prelude::*;
-use heron::*;
 use super::*;
 use crate::game::*;
+use bevy::prelude::*;
+use heron::*;
 use instant::Instant;
 use rand::prelude::Rng;
 
@@ -9,48 +9,94 @@ use rand::prelude::Rng;
 pub struct Cat;
 
 #[derive(Component)]
-pub struct MoveTime(Instant);
+pub struct MoveTime(pub Instant);
+
+#[derive(Component)]
+pub struct BadTime(pub Instant);
+
+#[derive(Component)]
+pub struct LookTime(pub Instant);
+
+#[derive(Component)]
+pub struct Bad(pub bool);
+
+#[derive(Component)]
+pub struct GoAway(pub bool);
 
 pub fn spawn_cats(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn_bundle(SpriteBundle {
-        texture: asset_server.load("test/cat_test.png"),
-        transform: Transform {
-            translation: Vec3::new(0.0, 0.0, 2.0),
+    commands
+        .spawn_bundle(SpriteBundle {
+            texture: asset_server.load("test/cat_test.png"),
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 2.0),
+                ..Default::default()
+            },
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(200.0, 200.0)),
+                ..Default::default()
+            },
             ..Default::default()
-        },
-        sprite: Sprite {
-            custom_size: Some(Vec2::new(200.0, 200.0)),
-            ..Default::default()
-        },
-        ..Default::default()
-    })
-    .insert(RigidBody::Static)
-    .insert(SensorShape)
-    .insert(CollisionShape::Sphere { radius: 20.0 })
-    .insert(GameMarker)
-    .insert(Cat)
-    .insert(Seen(false))
-    .insert(MoveTime(Instant::now()));
+        })
+        .insert(RigidBody::Static)
+        .insert(SensorShape)
+        .insert(CollisionShape::Sphere { radius: 50.0 })
+        .insert(GameMarker)
+        .insert(Cat)
+        .insert(Seen(false))
+        .insert(MoveTime(Instant::now()))
+        .insert(BadTime(Instant::now()))
+        .insert(LookTime(Instant::now()))
+        .insert(Bad(false))
+        .insert(GoAway(false));
 }
 
-pub fn cat_move(mut cats: Query<(&mut Transform, &mut MoveTime), With<Cat>>, places: Query<(&Transform, &Seen), (With<Place>, Without<Cat>)>) {
-    for (mut cat, mut time) in cats.iter_mut() {
-        if time.0.elapsed().as_millis() > 500 {
-            loop {
-                let mut rng = ::rand::thread_rng();
-                let mut new_place = rng.gen_range(0..places.iter().count());
-                for (place, seen) in places.iter() {
-                    new_place -= 1;
-                    
-                    if new_place == 0 {
-                        if seen.0 == false && place.translation != cat.translation {
-                            cat.translation = place.translation;
-                            time.0 = Instant::now();
-                            return;
+pub fn cat_move(
+    mut cats: Query<(&mut Transform, &mut MoveTime, &mut Bad, &mut BadTime, &mut GoAway), With<Cat>>,
+    places: Query<(&Transform, &Seen, Option<&BadPlace>), (With<Place>, Without<Cat>)>,
+) {
+    for (mut cat, mut time, mut bad, mut badtime, mut go_away) in cats.iter_mut() {
+        if go_away.0 || (time.0.elapsed().as_millis() > 500 && !bad.0) {
+            let mut rng = ::rand::thread_rng();
+            let mut new_place = rng.gen_range(0..places.iter().count() * 3);
+            for (place, seen, bad_place) in places.iter() {
+                if let (true, Some(_bp)) = (go_away.0, bad_place) {
+                    continue;
+                }
+                new_place -= 1;
+                if new_place == 0 {
+                    if seen.0 == false && place.translation != cat.translation {
+                        cat.translation = place.translation;
+                        if let Some(_bp) = bad_place {
+                            bad.0 = true;
+                            badtime.0 = Instant::now();
                         }
+                        go_away.0 = false;
+                        break;
                     }
                 }
             }
+            time.0 = Instant::now();
+        }
+    }
+}
+
+pub fn check_defeat(
+    cats: Query<(&mut Bad, &mut BadTime), With<Cat>>,
+    mut app_state: ResMut<State<AppState>>,
+) {
+    for (bad, bad_time) in cats.iter() {
+        if bad.0 && bad_time.0.elapsed().as_millis() > 3000 {
+            app_state.set(AppState::Start).unwrap();
+        }
+    }
+}
+
+pub fn go_away(mut cats: Query<(&mut Bad, &LookTime, &mut GoAway, &mut Seen), With<Cat>>) {
+    for (mut bad, look_time, mut go_away, mut seen) in cats.iter_mut() {
+        if bad.0 && seen.0 && look_time.0.elapsed().as_millis() > 1000 {
+            bad.0 = false;
+            go_away.0 = true;
+            seen.0 = false;
         }
     }
 }
--- src/game.rs
+++ src/game.rs
@@ -1,7 +1,8 @@
-use bevy::prelude::*;
-use heron::*;
 use super::*;
 use crate::cat::*;
+use bevy::prelude::*;
+use heron::*;
+use instant::Instant;
 
 #[derive(Component)]
 pub struct GameMarker;
@@ -25,175 +26,183 @@ pub struct Game;
 
 impl Plugin for Game {
     fn build(&self, app: &mut App) {
-        app
-            .add_system_set(
-                SystemSet::on_enter(AppState::Game)
+        app.add_system_set(
+            SystemSet::on_enter(AppState::Game)
                 .with_system(spawn_game)
-            .with_system(spawn_cats))
-            .add_system_set(
-                SystemSet::on_update(AppState::Game)
+                .with_system(spawn_cats),
+        )
+        .add_system_set(
+            SystemSet::on_update(AppState::Game)
                 .with_system(move_torch)
                 .with_system(check_collisions)
-                .with_system(cat_move))
-            .add_system_set(
-                SystemSet::on_exit(AppState::Game)
-                .with_system(cleanup_game)
-            );
+                .with_system(cat_move)
+                .with_system(check_defeat)
+                .with_system(go_away),
+        )
+        .add_system_set(SystemSet::on_exit(AppState::Game).with_system(cleanup_game));
     }
 }
 
 fn spawn_game(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn_bundle(SpriteBundle {
-        texture: asset_server.load("test/game_test.png"),
-        transform: Transform {
-            translation: Vec3::new(0.0, 0.0, 0.0),
+    commands
+        .spawn_bundle(SpriteBundle {
+            texture: asset_server.load("test/game_test.png"),
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 0.0),
+                ..Default::default()
+            },
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(800.0, 600.0)),
+                ..Default::default()
+            },
             ..Default::default()
-        },
-        sprite: Sprite {
-            custom_size: Some(Vec2::new(800.0, 600.0)),
-            ..Default::default()
-        },
-        ..Default::default()
-    })
-    .insert(GameMarker);
+        })
+        .insert(GameMarker);
 
-    commands.spawn_bundle(SpriteBundle {
-        texture: asset_server.load("test/torch_test.png"),
-        transform: Transform {
-            translation: Vec3::new(0.0, 0.0, -2.0),
+    commands
+        .spawn_bundle(SpriteBundle {
+            texture: asset_server.load("test/torch_test.png"),
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, -2.0),
+                ..Default::default()
+            },
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(3000.0, 2000.0)),
+                ..Default::default()
+            },
             ..Default::default()
-        },
-        sprite: Sprite {
-            custom_size: Some(Vec2::new(3000.0, 2000.0)),
-            ..Default::default()
-        },
-        ..Default::default()
-    })
-    .insert(RigidBody::Static)
-    .insert(SensorShape)
-    .insert(CollisionShape::Sphere { radius: 20.0 })
-    .insert(GameMarker)
-    .insert(Torch);
+        })
+        .insert(RigidBody::Static)
+        .insert(SensorShape)
+        .insert(CollisionShape::Sphere { radius: 20.0 })
+        .insert(GameMarker)
+        .insert(Torch);
 
-    commands.spawn_bundle(SpriteBundle {
-        transform: Transform {
-            translation: Vec3::new(100.0, 100.0, 2.0),
-            scale: Vec3::new(100.0, 100.0, 0.0),
+    commands
+        .spawn_bundle(SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(100.0, 100.0, 2.0),
+                scale: Vec3::new(100.0, 100.0, 0.0),
+                ..Default::default()
+            },
+            sprite: Sprite {
+                color: Color::BLUE,
+                ..Default::default()
+            },
             ..Default::default()
-        },
-        sprite: Sprite {
-            color: Color::BLUE,
-            ..Default::default()
-        },
-        ..Default::default()
-    })
-    .insert(RigidBody::Static)
-    .insert(SensorShape)
-    .insert(CollisionShape::Sphere { radius: 20.0 })
-    .insert(GameMarker)
-    .insert(GoodPlace)
-    .insert(Place)
-    .insert(Seen(false));
+        })
+        .insert(RigidBody::Static)
+        .insert(SensorShape)
+        .insert(CollisionShape::Sphere { radius: 20.0 })
+        .insert(GameMarker)
+        .insert(GoodPlace)
+        .insert(Place)
+        .insert(Seen(false));
 
-    commands.spawn_bundle(SpriteBundle {
-        transform: Transform {
-            translation: Vec3::new(200.0, -100.0, 2.0),
-            scale: Vec3::new(100.0, 100.0, 0.0),
+    commands
+        .spawn_bundle(SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(200.0, -100.0, 2.0),
+                scale: Vec3::new(100.0, 100.0, 0.0),
+                ..Default::default()
+            },
+            sprite: Sprite {
+                color: Color::BLUE,
+                ..Default::default()
+            },
             ..Default::default()
-        },
-        sprite: Sprite {
-            color: Color::BLUE,
-            ..Default::default()
-        },
-        ..Default::default()
-    })
-    .insert(RigidBody::Static)
-    .insert(SensorShape)
-    .insert(CollisionShape::Sphere { radius: 20.0 })
-    .insert(GameMarker)
-    .insert(GoodPlace)
-    .insert(Place)
-    .insert(Seen(false));
+        })
+        .insert(RigidBody::Static)
+        .insert(SensorShape)
+        .insert(CollisionShape::Sphere { radius: 20.0 })
+        .insert(GameMarker)
+        .insert(GoodPlace)
+        .insert(Place)
+        .insert(Seen(false));
 
-    commands.spawn_bundle(SpriteBundle {
-        transform: Transform {
-            translation: Vec3::new(-100.0, 100.0, 2.0),
-            scale: Vec3::new(100.0, 100.0, 0.0),
+    commands
+        .spawn_bundle(SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(-100.0, 100.0, 2.0),
+                scale: Vec3::new(100.0, 100.0, 0.0),
+                ..Default::default()
+            },
+            sprite: Sprite {
+                color: Color::BLUE,
+                ..Default::default()
+            },
             ..Default::default()
-        },
-        sprite: Sprite {
-            color: Color::BLUE,
-            ..Default::default()
-        },
-        ..Default::default()
-    })
-    .insert(RigidBody::Static)
-    .insert(SensorShape)
-    .insert(CollisionShape::Sphere { radius: 20.0 })
-    .insert(GameMarker)
-    .insert(GoodPlace)
-    .insert(Place)
-    .insert(Seen(false));
+        })
+        .insert(RigidBody::Static)
+        .insert(SensorShape)
+        .insert(CollisionShape::Sphere { radius: 20.0 })
+        .insert(GameMarker)
+        .insert(GoodPlace)
+        .insert(Place)
+        .insert(Seen(false));
 
-    commands.spawn_bundle(SpriteBundle {
-        transform: Transform {
-            translation: Vec3::new(-200.0, -100.0, 2.0),
-            scale: Vec3::new(100.0, 100.0, 0.0),
+    commands
+        .spawn_bundle(SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(-200.0, -100.0, 2.0),
+                scale: Vec3::new(100.0, 100.0, 0.0),
+                ..Default::default()
+            },
+            sprite: Sprite {
+                color: Color::RED,
+                ..Default::default()
+            },
             ..Default::default()
-        },
-        sprite: Sprite {
-            color: Color::RED,
-            ..Default::default()
-        },
-        ..Default::default()
-    })
-    .insert(RigidBody::Static)
-    .insert(SensorShape)
-    .insert(CollisionShape::Sphere { radius: 20.0 })
-    .insert(GameMarker)
-    .insert(BadPlace)
-    .insert(Place)
-    .insert(Seen(false));
+        })
+        .insert(RigidBody::Static)
+        .insert(SensorShape)
+        .insert(CollisionShape::Sphere { radius: 20.0 })
+        .insert(GameMarker)
+        .insert(BadPlace)
+        .insert(Place)
+        .insert(Seen(false));
 
-    commands.spawn_bundle(SpriteBundle {
-        transform: Transform {
-            translation: Vec3::new(-400.0, -100.0, 2.0),
-            scale: Vec3::new(100.0, 100.0, 0.0),
+    commands
+        .spawn_bundle(SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(-400.0, -100.0, 2.0),
+                scale: Vec3::new(100.0, 100.0, 0.0),
+                ..Default::default()
+            },
+            sprite: Sprite {
+                color: Color::RED,
+                ..Default::default()
+            },
             ..Default::default()
-        },
-        sprite: Sprite {
-            color: Color::RED,
+        })
+        .insert(RigidBody::Static)
+        .insert(SensorShape)
+        .insert(CollisionShape::Sphere { radius: 20.0 })
+        .insert(GameMarker)
+        .insert(BadPlace)
+        .insert(Place)
+        .insert(Seen(false));
+
+    commands
+        .spawn_bundle(SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(-200.0, 300.0, 2.0),
+                scale: Vec3::new(100.0, 100.0, 0.0),
+                ..Default::default()
+            },
+            sprite: Sprite {
+                color: Color::RED,
+                ..Default::default()
+            },
             ..Default::default()
-        },
-        ..Default::default()
-    })
-    .insert(RigidBody::Static)
-    .insert(SensorShape)
-    .insert(CollisionShape::Sphere { radius: 20.0 })
-    .insert(GameMarker)
-    .insert(BadPlace)
-    .insert(Place)
-    .insert(Seen(false));
-    
-    commands.spawn_bundle(SpriteBundle {
-        transform: Transform {
-            translation: Vec3::new(-200.0, 300.0, 2.0),
-            scale: Vec3::new(100.0, 100.0, 0.0),
-            ..Default::default()
-        },
-        sprite: Sprite {
-            color: Color::RED,
-            ..Default::default()
-        },
-        ..Default::default()
-    })
-    .insert(RigidBody::Static)
-    .insert(SensorShape)
-    .insert(CollisionShape::Sphere { radius: 20.0 })
-    .insert(GameMarker)
-    .insert(BadPlace)
-    .insert(Place)
-    .insert(Seen(false));
+        })
+        .insert(RigidBody::Static)
+        .insert(SensorShape)
+        .insert(CollisionShape::Sphere { radius: 20.0 })
+        .insert(GameMarker)
+        .insert(BadPlace)
+        .insert(Place)
+        .insert(Seen(false));
 }
 
 fn cleanup_game(mut commands: Commands, query: Query<Entity, With<GameMarker>>) {
@@ -202,9 +211,11 @@ fn cleanup_game(mut commands: Commands, query: Query<Entity, With<GameMarker>>) 
     }
 }
 
-fn move_torch(mut torch: Query<&mut Transform, With<Torch>>, wnds: Res<Windows>,
-    q_camera: Query<(&Camera, &GlobalTransform), With<MainCamera>>) {
-    
+fn move_torch(
+    mut torch: Query<&mut Transform, With<Torch>>,
+    wnds: Res<Windows>,
+    q_camera: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
+) {
     let mut torch = torch.single_mut();
     let (camera, camera_transform) = q_camera.single();
     let wnd = wnds.get(camera.window).unwrap();
@@ -221,28 +232,53 @@ fn move_torch(mut torch: Query<&mut Transform, With<Torch>>, wnds: Res<Windows>,
     }
 }
 
-fn check_collisions(mut events: EventReader<CollisionEvent>, torch: Query<Entity, With<Torch>>,
-    mut good_places: Query<(Entity, &mut Transform, &mut Seen), With<Place>>) {
-        let torch = torch.single();
-        for event in events.iter() {
-            match event {
-                CollisionEvent::Started(t, some_place) if t.rigid_body_entity() == torch => {
-                    for (place, mut place_transform, mut seen) in good_places.iter_mut() {
-                        if place == some_place.rigid_body_entity() {
+fn check_collisions(
+    mut events: EventReader<CollisionEvent>,
+    torch: Query<Entity, With<Torch>>,
+    mut good_places: Query<(
+        Entity,
+        &mut Transform,
+        &mut Seen,
+        Option<&Place>,
+        Option<&Bad>,
+        Option<&mut LookTime>,
+    )>,
+) {
+    let torch = torch.single();
+    for event in events.iter() {
+        match event {
+            CollisionEvent::Started(t, some_place) if t.rigid_body_entity() == torch => {
+                for (place, mut place_transform, mut seen, is_place, bad, mut look_time) in
+                    good_places.iter_mut()
+                {
+                    if place == some_place.rigid_body_entity() {
+                        if let Some(_is_place) = is_place {
                             place_transform.scale = Vec3::new(10.0, 10.0, 0.0);
-                            seen.0 = true;
                         }
+                        if let Some(mut look_time) = look_time {
+                            if let Some(bad) = bad {
+                                if bad.0 {
+                                    look_time.0 = Instant::now();
+                                }
+                            }
+                        }
+                        seen.0 = true;
                     }
-                },
-                CollisionEvent::Stopped(t, some_place) if t.rigid_body_entity() == torch => {
-                    for (place, mut place_transform, mut seen) in good_places.iter_mut() {
-                        if place == some_place.rigid_body_entity() {
+                }
+            }
+            CollisionEvent::Stopped(t, some_place) if t.rigid_body_entity() == torch => {
+                for (place, mut place_transform, mut seen, is_place, _bad, _look_time) in
+                    good_places.iter_mut()
+                {
+                    if place == some_place.rigid_body_entity() {
+                        if let Some(_is_place) = is_place {
                             place_transform.scale = Vec3::new(100.0, 100.0, 0.0);
-                            seen.0 = false;
                         }
+                        seen.0 = false;
                     }
-                },
-                _ => ()
-            } 
+                }
+            }
+            _ => (),
         }
     }
+}
